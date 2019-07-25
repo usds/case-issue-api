7f7ff917db61608f9ba341605f8b05ecb6135bda
Ben Warfield <ben.warfield@usds.dhs.gov>
2019-07-25 16:58:57 -0400

    Added active issues and random details to TroubleCase.
    Added a JSON-mapped dictionary of arbitary other fields to TroubleCase,
    along with the list of un-closed CaseIssues, allowing for the an update
    to the repository to fetch cases with active issues, and filter by
    whether or not those cases have active snoozes.

--- src/main/java/gov/usds/case_issues/db/model/TroubleCase.java
+++ src/main/java/gov/usds/case_issues/db/model/TroubleCase.java
@@ -1,17 +1,26 @@
 package gov.usds.case_issues.db.model;
 
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.Where;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 
 /**
  * A case that has been seen, at some point, by this system
@@ -19,6 +28,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  */
 /* And yes, "Case" would be a simpler name, until you remember that it's a reserved word in every language ever */
 @Entity
+@TypeDef(name="json", typeClass=JsonStringType.class)
 public class TroubleCase {
 
 	@Id
@@ -39,16 +49,26 @@ public class TroubleCase {
 	@NotNull
 	private ZonedDateTime caseCreation;
 
+	@OneToMany(mappedBy="issueCase") // that right?
+	@Where(clause="issue_closed is null")
+	private List<CaseIssue> openIssues;
+
+	@Type(type="json")
+	@Column(columnDefinition = "varchar(32000)")
+	private Map<String, Object> extraData;
+
 	protected TroubleCase() {/* for hibernate/JPA */}
 
 	public TroubleCase(CaseManagementSystem caseManagementSystem,
 			@NotNull @Pattern(regexp = "[-\\w]+") String receiptNumber, CaseType caseType,
-			@NotNull ZonedDateTime caseCreation) {
+			@NotNull ZonedDateTime caseCreation,
+			Map<String,Object> extraData) {
 		this();
 		this.caseManagementSystem = caseManagementSystem;
 		this.receiptNumber = receiptNumber;
 		this.caseType = caseType;
 		this.caseCreation = caseCreation;
+		this.extraData = new HashMap<String, Object>(extraData); // shallow copy for reasonable safety
 	}
 
 	public Long getInternalCaseId() {
@@ -69,5 +89,13 @@ public class TroubleCase {
 
 	public ZonedDateTime getCaseCreation() {
 		return caseCreation;
+	}
+
+	public List<CaseIssue> getOpenIssues() {
+		return openIssues;
+	}
+
+	public Map<String, Object> getExtraData() {
+		return extraData;
 	}
 }
--- src/main/java/gov/usds/case_issues/db/repositories/TroubleCaseRepository.java
+++ src/main/java/gov/usds/case_issues/db/repositories/TroubleCaseRepository.java
@@ -1,7 +1,10 @@
 package gov.usds.case_issues.db.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -18,6 +21,22 @@ import gov.usds.case_issues.db.model.TroubleCase;
 )
 public interface TroubleCaseRepository extends PagingAndSortingRepository<TroubleCase, Long> {
 
+	public static final String ACTIVE_CASE_CLAUSE = "c.caseManagementSystem = :caseManagementSystem and c.caseType = :caseType and c.openIssues is not empty";
+	public static final String ACTIVE_CASE_QUERY = "select c from #{#entityName} c where " + ACTIVE_CASE_CLAUSE;
+	public static final String ACTIVE_SNOOZE_CLAUSE = "exists (select snoozeEnd from CaseSnooze where snoozeCase = c and snoozeEnd > CURRENT_TIMESTAMP)";
+
 	public Page<TroubleCase> getAllByCaseManagementSystemAndCaseType(CaseManagementSystem caseManager, CaseType caseType, Pageable pageable);
+
+	public Optional<TroubleCase> findByCaseManagementSystemAndReceiptNumber(CaseManagementSystem caseManager, String receiptNumber);
+
+	@Query(ACTIVE_CASE_QUERY)
+	public Page<TroubleCase> getWithOpenIssues(CaseManagementSystem caseManagementSystem, CaseType caseType, Pageable pageable);
+
+	@Query(ACTIVE_CASE_QUERY + " and not " + ACTIVE_SNOOZE_CLAUSE)
+	public Page<TroubleCase> getAwakeWithOpenIssues(CaseManagementSystem caseManagementSystem, CaseType caseType, Pageable pageable);
+
+	// we will want to fetch the snooze info in a join: need to figure out how DTO works
+	@Query(ACTIVE_CASE_QUERY + " and " + ACTIVE_SNOOZE_CLAUSE)
+	public Page<TroubleCase> getSnoozedWithOpenIssues(CaseManagementSystem caseManagementSystem, CaseType caseType, Pageable pageable);
 
 }
