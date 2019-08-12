8d083665ec3ac0dd5967eaf7c564843e76cbb3db
Ben Warfield <ben.warfield@usds.dhs.gov>
2019-08-12 00:29:34 -0400

    Added very bad PUT endpoint for case lists.
    Added a PUT endpoint that accepts a list of unrestricted JSON objects and assumes
    a great deal too much about their contents before calling the service method that
    will turn those objects into case/issue tuples in the database.

--- src/main/java/gov/usds/case_issues/controllers/HitlistApiController.java
+++ src/main/java/gov/usds/case_issues/controllers/HitlistApiController.java
@@ -1,17 +1,27 @@
 package gov.usds.case_issues.controllers;
 
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import gov.usds.case_issues.model.CaseRequest;
 import gov.usds.case_issues.model.CaseSummary;
 import gov.usds.case_issues.services.CaseListService;
 import io.swagger.annotations.ApiImplicitParam;
@@ -51,5 +61,45 @@ public class HitlistApiController {
 	@RequestMapping(value="summary", method=RequestMethod.GET)
 	public Map<String, Number> getSummary(@PathVariable String caseManagementSystemTag, @PathVariable String caseTypeTag) {
 		return _listService.getSummaryInfo(caseManagementSystemTag, caseTypeTag);
+	}
+
+	@PutMapping(value="/{issueTag}",consumes= {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<?> updateIssueListJson(@PathVariable String caseManagementSystemTag, @PathVariable String caseTypeTag, @PathVariable String issueTag,
+			@RequestBody List<Map<String,Object>> csvStream) throws IOException {
+		Iterator<Map<String,Object>> valueIterator = csvStream.listIterator();
+		List<CaseRequest> newIssueCases = new ArrayList<>();
+		valueIterator.forEachRemaining(m -> {
+			newIssueCases.add(new MapBasedCaseRequest(m));
+		});
+		_listService.putIssueList(caseManagementSystemTag, caseTypeTag, issueTag, newIssueCases, ZonedDateTime.now());
+		return ResponseEntity.accepted().build();
+	}
+
+	private static class MapBasedCaseRequest implements CaseRequest {
+
+		private String _receipt;
+		private ZonedDateTime _caseCreation;
+		private Map<String, Object> _rest;
+
+		public MapBasedCaseRequest(Map<String, Object> input) {
+			_receipt = input.remove("receiptNumber").toString();
+			_caseCreation = ZonedDateTime.parse(input.remove("creationDate").toString(), DateTimeFormatter.ISO_DATE_TIME);
+			_rest = input;
+		}
+
+		@Override
+		public String getReceiptNumber() {
+			return _receipt;
+		}
+
+		@Override
+		public ZonedDateTime getCaseCreation() {
+			return _caseCreation;
+		}
+
+		@Override
+		public Map<String, Object> getExtraData() {
+			return _rest;
+		}
 	}
 }
