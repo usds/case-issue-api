8860bda314e0a8bb3ca135c264631a1a7de0e3aa
Ben Warfield <ben.warfield@usds.dhs.gov>
2019-08-13 11:08:58 -0400

    Cleaned up auto-discovery of bulk repo methods.
    The named-query repository methods in BulkCaseRepository really should not be
    accessible in the HAL browser: hiding them.

--- src/main/java/gov/usds/case_issues/db/repositories/BulkCaseRepository.java
+++ src/main/java/gov/usds/case_issues/db/repositories/BulkCaseRepository.java
@@ -5,21 +5,23 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Repository interface for bulk operations (not independently autowired because
  * this turns out to break things horribly).
  */
-@RepositoryRestResource(exported=false)
 public interface BulkCaseRepository {
 
 	@Query(name="snoozed")
+	@RestResource(exported=false)
 	public Page<Object[]> getSnoozedCases(Long caseManagementSystemId, Long caseTypeId, Pageable p);
 
 	@Query(name="unSnoozed")
+	@RestResource(exported=false)
 	public Page<Object[]> getActiveCases(Long caseManagementSystemId, Long caseTypeId, Pageable p);
 
 	@Query(name="summary")
+	@RestResource(exported=false)
 	public List<Object[]> getSnoozeSummary(Long caseManagementSystemId, Long caseTypeId);
 }
