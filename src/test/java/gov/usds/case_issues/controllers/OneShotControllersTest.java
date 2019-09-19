da87f373239a28003b9738882056f5d903d12289
Ben Warfield <ben.warfield@usds.dhs.gov>
2019-09-19 17:38:48 -0400

    Mild refactor/rearrangement of one-shot tests.

--- src/test/java/gov/usds/case_issues/controllers/OneShotControllersTest.java
+++ src/test/java/gov/usds/case_issues/controllers/OneShotControllersTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 /**
  * Consolidated tests for the controllers that have one handler each and are too annoying.
@@ -15,16 +16,24 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("auth-testing")
 public class OneShotControllersTest extends ControllerTestBase {
 
+	private static MockHttpServletRequestBuilder getUser() {
+		return get("/user");
+	}
+
+	private static MockHttpServletRequestBuilder getCsrf() {
+		return get("/csrf");
+	}
+
 	@Test
 	@WithAnonymousUser
 	public void getUser_anonymous_forbidden() throws Exception {
-		perform(get("/user")).andExpect(status().isForbidden());
+		perform(getUser()).andExpect(status().isForbidden());
 	}
 
 	@Test
 	@WithMockUser(username="Freddie Fixer", authorities={"RESPECT", "FIGURE"})
 	public void getUser_loggedInUser_expectedResult() throws Exception {
-		perform(get("/user"))
+		perform(getUser())
 			.andExpect(status().isOk())
 			.andExpect(content().json("{\"name\": \"Freddie Fixer\", \"authorities\":[{\"authority\": \"FIGURE\"},{\"authority\": \"RESPECT\"}]}"))
 			;
@@ -33,7 +42,7 @@ public class OneShotControllersTest extends ControllerTestBase {
 	@Test
 	@WithMockUser(username="Freddie Fixer", authorities={"RESPECT", "FIGURE"})
 	public void getUser_loggedInUserOkOrigin_okResult() throws Exception {
-		perform(get("/user").header("Origin", ORIGIN_HTTPS_OK))
+		perform(getUser().header("Origin", ORIGIN_HTTPS_OK))
 			.andExpect(status().isOk())
 			;
 	}
@@ -41,27 +50,30 @@ public class OneShotControllersTest extends ControllerTestBase {
 	@Test
 	@WithMockUser(username="Freddie Fixer", authorities={"RESPECT", "FIGURE"})
 	public void getUser_loggedInUserBadOrigin_forbidden() throws Exception {
-		perform(get("/user").header("Origin", ORIGIN_NOT_OK))
+		perform(getUser().header("Origin", ORIGIN_NOT_OK))
 			.andExpect(status().isForbidden())
 			;
 	}
 
 	@Test
 	@WithAnonymousUser
+	// we allow this because it's convenient for users of Swagger-UI in the dev environment
+	// it is not clear that this is sufficent justification, though also it's not clear that
+	// it matters much (could spam this URL to create a lot of dead sessions, I guess?)
 	public void getCsrf_anonymous_okResult() throws Exception {
-		perform(get("/csrf"))
+		perform(getCsrf()).andExpect(status().isOk());
+	}
+
+	@Test
+	public void getCsrf_okOrigin_okResult() throws Exception {
+		perform(getCsrf().header("Origin", ORIGIN_HTTP_OK))
 			.andExpect(status().isOk())
 			.andExpect(content().json("{\"headerName\": \"X-CSRF-TOKEN\"}")) // matching on the token is not worth it
 			;
 	}
 
 	@Test
-	public void getCsrf_okOrigin_okResult() throws Exception {
-		perform(get("/csrf").header("Origin", ORIGIN_HTTP_OK)).andExpect(status().isOk());
-	}
-
-	@Test
 	public void getCsrf_badOrigin_forbidden() throws Exception {
-		perform(get("/csrf").header("Origin", ORIGIN_NOT_OK)).andExpect(status().isForbidden());
+		perform(getCsrf().header("Origin", ORIGIN_NOT_OK)).andExpect(status().isForbidden());
 	}
 }
