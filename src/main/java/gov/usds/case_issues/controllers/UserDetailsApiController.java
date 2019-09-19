cee2e05c422abbaa3d07bbe59674e53d1d572ae1
Tim Best <timothy.best@hq.dhs.gov>
2019-09-19 09:37:57 -0400

    return entire usre object for now

--- src/main/java/gov/usds/case_issues/controllers/UserDetailsApiController.java
+++ src/main/java/gov/usds/case_issues/controllers/UserDetailsApiController.java
@@ -1,7 +1,5 @@
 package gov.usds.case_issues.controllers;
 
-import java.util.HashMap;
-
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +16,6 @@ public class UserDetailsApiController {
 
 	@GetMapping
 	public Object getUser(Authentication u) {
-		HashMap<String, String> user = new HashMap<String, String>();
-		user.put("Name", u.getName());
-		return user;
+		return u;
 	}
 }
