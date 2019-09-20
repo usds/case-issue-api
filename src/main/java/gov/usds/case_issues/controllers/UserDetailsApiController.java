0c3d0008856df501d7a8932bbe64e135500cb6b9
Tim Best <timothy.best@hq.dhs.gov>
2019-09-20 17:06:32 -0400

    clean up

--- src/main/java/gov/usds/case_issues/controllers/UserDetailsApiController.java
+++ src/main/java/gov/usds/case_issues/controllers/UserDetailsApiController.java
@@ -4,39 +4,37 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller to see information about the current user.
  */
 @RestController
-@RequestMapping(UserDetailsApiController.USER_INFO_ENDPOINT)
 @Profile("auth-testing")
 public class UserDetailsApiController {
-	private static final Logger LOG = LoggerFactory.getLogger(UserDetailsApiController.class);
 
 	public static final String USER_INFO_ENDPOINT = "/api/users";
 
-	@GetMapping
+	@GetMapping(UserDetailsApiController.USER_INFO_ENDPOINT)
 	@SuppressWarnings({"unchecked"})
 	public Object getUser(Authentication u) {
 		HashMap<String, String> user = new HashMap<String, String>();
 		user.put("ID", u.getName());
-		try {
-			Map<String, Object> principal = (Map<String, Object>)u.getPrincipal();
-			Collection<Object> authorities = (Collection<Object>)principal.get("authorities");
-			Map<String, Object> authority = (Map<String, Object>)authorities.iterator().next();
-			Map<String, String> attributes = (Map<String, String>)authority.get("attributes");
-			user.put("name", attributes.get("name"));
-		} catch(Exception e) {
-			LOG.error("Unable to get user name", e);
-		}
+		Map<String, Object> principal = (Map<String, Object>)u.getPrincipal();
+		Collection<Object> authorities = (Collection<Object>)principal.get("authorities");
+		Map<String, Object> authority = (Map<String, Object>)authorities.iterator().next();
+		Map<String, String> attributes = (Map<String, String>)authority.get("attributes");
+		user.put("name", attributes.get("name"));
 		return user;
+	}
+
+	@GetMapping(UserDetailsApiController.USER_INFO_ENDPOINT + "/loggedin")
+	public ResponseEntity<?> getUserLoggedin() {
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 }
