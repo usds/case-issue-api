f0f7e366e5fb4e666d7c6fa0f2c243de0aa6b7ad
Tim Best <timothy.best@hq.dhs.gov>
2019-09-30 15:31:55 -0400

    keyset paging

--- src/main/java/gov/usds/case_issues/db/repositories/BulkCaseRepository.java
+++ src/main/java/gov/usds/case_issues/db/repositories/BulkCaseRepository.java
@@ -1,11 +1,12 @@
 package gov.usds.case_issues.db.repositories;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RestResource;
+
+import gov.usds.case_issues.db.model.TroubleCase;
 
 /**
  * Repository interface for bulk operations (not independently autowired because
@@ -15,11 +16,36 @@ public interface BulkCaseRepository {
 
 	@Query(name="snoozed")
 	@RestResource(exported=false)
-	public Page<Object[]> getSnoozedCases(Long caseManagementSystemId, Long caseTypeId, Pageable p);
+	public List<Object[]> getSnoozedCases(Long caseManagementSystemId, Long caseTypeId, Integer size);
+
+	@Query(name="snoozedAfter")
+	@RestResource(exported=false)
+	public List<Object[]> getSnoozedCasesAfter(
+		Long caseManagementSystemId,
+		Long caseTypeId,
+		ZonedDateTime lastSnoozeEnd,
+		ZonedDateTime caseCreation,
+		Long internalId,
+		Integer size
+	);
 
 	@Query(name="unSnoozed")
 	@RestResource(exported=false)
-	public Page<Object[]> getActiveCases(Long caseManagementSystemId, Long caseTypeId, Pageable p);
+	public List<Object[]> getActiveCases(
+		Long caseManagementSystemId,
+		Long caseTypeId,
+		Integer size
+	);
+
+	@Query(name="unSnoozedAfter")
+	@RestResource(exported=false)
+	public List<Object[]> getActiveCasesAfter(
+		Long caseManagementSystemId,
+		Long caseTypeId,
+		ZonedDateTime caseCreation,
+		Long internalId,
+		Integer size
+	);
 
 	@Query(name="summary")
 	@RestResource(exported=false)
