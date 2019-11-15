03ad35982a0798f55397b2e639dcafc8ac247c77
Tim Best <timothy.best@hq.dhs.gov>
2019-11-15 17:23:52 -0500

    Cache calls to getUserById
    This prevents multiple user selects from being preformed on GET calls to case details
    
    Note: this is an application runtime level cache. It will be cleared everytime
    the application resets

--- src/main/java/gov/usds/case_issues/db/repositories/UserRepository.java
+++ src/main/java/gov/usds/case_issues/db/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package gov.usds.case_issues.db.repositories;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 import gov.usds.case_issues.db.model.UserInformation;;
@@ -7,5 +9,14 @@ import gov.usds.case_issues.db.model.UserInformation;;
 
 public interface UserRepository extends CrudRepository<UserInformation, Long> {
 
+	@Override
+	@CacheEvict("byUserId")
+	<S extends UserInformation> S save(S entity);
+
+	@Override
+	@CacheEvict("byUserId")
+	void delete(UserInformation entity);
+
+	@Cacheable(value="byUserId", key="#userId")
 	public UserInformation findByUserId(String userId);
 }
