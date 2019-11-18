e0636803f0d553b944bd634b92e50d553b182cd0
Tim Best <timothy.best@hq.dhs.gov>
2019-11-18 13:10:07 -0500

    Correctly set chache keys
    icreateUserOrUpdateLastSeen was checking if the users existed by calling
    findByUserId this set the cache to null and the cache was not being evited
    by the save method because the incorect cache key was used

--- src/main/java/gov/usds/case_issues/db/repositories/UserRepository.java
+++ src/main/java/gov/usds/case_issues/db/repositories/UserRepository.java
@@ -10,11 +10,11 @@ import gov.usds.case_issues.db.model.UserInformation;;
 public interface UserRepository extends CrudRepository<UserInformation, Long> {
 
 	@Override
-	@CacheEvict("byUserId")
+	@CacheEvict(value="byUserId", key="#entity.getId()")
 	<S extends UserInformation> S save(S entity);
 
 	@Override
-	@CacheEvict("byUserId")
+	@CacheEvict(value="byUserId", key="#entity.getId()")
 	void delete(UserInformation entity);
 
 	@Cacheable(value="byUserId", key="#userId")
