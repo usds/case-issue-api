e946ea4d22a67a249d9350167eebd26218990047
Ben Warfield <ben.warfield@usds.dhs.gov>
2019-12-27 11:03:53 -0500

    Added typdef for "jsonb" columns.
    Added a global typedef using the JsonBinaryType class, and made it the
    default for Map fields (still explicitly overridden for the legacy case).

--- src/main/java/gov/usds/case_issues/db/model/package-info.java
+++ src/main/java/gov/usds/case_issues/db/model/package-info.java
@@ -1,4 +1,14 @@
 /**
  * Hibernate/JPA models.
  */
+@TypeDefs({
+	@TypeDef(defaultForType=Map.class, typeClass=JsonBinaryType.class, name="jsonb"),
+})
 package gov.usds.case_issues.db.model;
+
+import java.util.Map;
+
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
