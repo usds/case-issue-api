dbc1e180e4695e42d867816b631a7ee9833890b7
Tim Best <timothy.best@hq.dhs.gov>
2020-01-10 17:17:43 -0500

    update path to CaseGroupInfo

--- src/main/java/gov/usds/case_issues/services/KPIService.java
+++ src/main/java/gov/usds/case_issues/services/KPIService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import gov.usds.case_issues.db.repositories.BulkCaseRepository;
-import gov.usds.case_issues.services.CaseListService.CaseGroupInfo;
+import gov.usds.case_issues.services.model.CaseGroupInfo;
 import gov.usds.case_issues.validators.TagFragment;
 
 @Service
