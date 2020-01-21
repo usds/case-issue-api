4d57670a8cc4e4686253e353dbe44f50611a3938
Tim Best <timothy.best@hq.dhs.gov>
2020-01-21 11:53:14 -0500

    remove profile restriction

--- src/main/java/gov/usds/case_issues/controllers/KPIController.java
+++ src/main/java/gov/usds/case_issues/controllers/KPIController.java
@@ -3,7 +3,6 @@ package gov.usds.case_issues.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ import gov.usds.case_issues.services.KPIService;
  * Controller to see information about the current user.
  */
 @RestController
-@Profile("auth-testing")
 @RequestMapping("/api/reporting/{caseManagementSystemTag}/{caseTypeTag}")
 public class KPIController {
 
