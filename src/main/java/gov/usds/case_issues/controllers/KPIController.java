e672d1dfaf9515a9d6ccdcb66d6765b488a3a374
Tim Best <timothy.best@hq.dhs.gov>
2020-02-07 11:37:38 -0500

    pull dat from any start date

--- src/main/java/gov/usds/case_issues/controllers/KPIController.java
+++ src/main/java/gov/usds/case_issues/controllers/KPIController.java
@@ -1,11 +1,15 @@
 package gov.usds.case_issues.controllers;
 
+import java.time.ZonedDateTime;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gov.usds.case_issues.services.KPIService;
@@ -24,10 +28,12 @@ public class KPIController {
 	@GetMapping
 	public Map<String, Object> getAppPerformanceMetrics(
 		@PathVariable String caseManagementSystemTag,
-		@PathVariable String caseTypeTag
+		@PathVariable String caseTypeTag,
+		@RequestParam(required=false) @DateTimeFormat(iso=ISO.DATE_TIME) ZonedDateTime start
 	) {
-		return _KPIService.getKPIData(caseManagementSystemTag, caseTypeTag);
+		if (start == null) {
+			return _KPIService.getKPIData(caseManagementSystemTag, caseTypeTag, ZonedDateTime.now());
+		}
+		return _KPIService.getKPIData(caseManagementSystemTag, caseTypeTag, start);
 	}
-
-
 }
