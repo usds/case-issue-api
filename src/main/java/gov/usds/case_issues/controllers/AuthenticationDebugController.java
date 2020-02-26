336f66d6410a2f728e3fd299f2981d0912d9c8b3
Ben Warfield <ben.warfield@usds.dhs.gov>
2020-02-25 22:27:32 -0500

    Added probably excessive auth debugging options.
    Supplemented the auth-info controller with a couple more methods that tell us
    more about the implementation details of the current user authentication than
    it is necessarily prudent for us to know.

--- src/main/java/gov/usds/case_issues/controllers/AuthenticationDebugController.java
+++ src/main/java/gov/usds/case_issues/controllers/AuthenticationDebugController.java
@@ -1,5 +1,13 @@
 package gov.usds.case_issues.controllers;
 
+import java.lang.reflect.Method;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +27,48 @@ import springfox.documentation.annotations.ApiIgnore;
 public class AuthenticationDebugController {
 
 	@GetMapping
-	public Object getUser(Authentication u) {
+	public Authentication getUser(Authentication u) {
 		return u;
 	}
+
+	@GetMapping("details")
+	public Map<String, ?> getDetails(Authentication auth, HttpServletRequest req) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("authentication", auth);
+		map.put("authType", req.getAuthType());
+		map.put("requestPrincipal", req.getUserPrincipal());
+		HttpSession session = req.getSession(false);
+		map.put("sessionId", session == null ? null : session.getId());
+		return map;
+	}
+
+	@GetMapping("types")
+	public Map<String, ?> getTypes(Authentication auth, HttpServletRequest req) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("authenticationClass", auth.getClass());
+		Object principal = auth.getPrincipal();
+		map.put("authPrincipalClass", principal.getClass());
+		try {
+			map.put("authNestedPrincipalClass", getSubPrincipal(principal).getClass());
+		} catch (SecurityException | ReflectiveOperationException | IllegalArgumentException  e) {
+			map.put("authNestedPrincipalExceptionClass", e.getClass());
+		}
+		Principal userPrincipal = req.getUserPrincipal();
+		map.put("requestPrincipalClass", userPrincipal.getClass());
+		try {
+			map.put("requestNestedPrincipalClass", getSubPrincipal(userPrincipal).getClass());
+		} catch (SecurityException | ReflectiveOperationException | IllegalArgumentException  e) {
+			map.put("requestNestedPrincipalExceptionClass", e.getClass());
+		}
+		HttpSession session = req.getSession(false);
+		map.put("sessionClass", session != null ? session.getClass() : null);
+		return map;
+
+	}
+
+	private Object getSubPrincipal(Object principal) throws ReflectiveOperationException, IllegalArgumentException  {
+		Method m = principal.getClass().getMethod("getPrincipal");
+		return m.invoke(principal);
+	}
+
 }
