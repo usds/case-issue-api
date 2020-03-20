a75d3543908060a0a86e871cbc8e05569a1ebab1
Ben Warfield <ben.warfield@usds.dhs.gov>
2020-03-19 22:40:09 -0400

    Unused import.

--- src/test/java/gov/usds/case_issues/controllers/ControllerTestBase.java
+++ src/test/java/gov/usds/case_issues/controllers/ControllerTestBase.java
@@ -2,7 +2,6 @@ package gov.usds.case_issues.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
